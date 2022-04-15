32ccdc51cf2fb8e2066b9983f44a426ab0b244ae
nvlhnn <hanannaufal17@gmail.com>
2022-04-15 17:30:31 +0700

    update project

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const routes = require("./routes");
+const path = require("path");
 
 dotenv.config();
 
@@ -18,6 +19,16 @@ app.use(express.json());
 require("./config/passport")(app);
 
 app.use(routes);
+
+// ______________deployment_____________
+__dirname = path.resolve();
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(path.join(__dirname, "/client/build")));
+
+  app.use("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+  });
+}
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("conected to port 5000");
