09339ee196fdef9f4025191309481fe770bfa3ea
nvlhnn <hanannaufal17@gmail.com>
2022-04-15 16:42:21 +0700

    update project

---
+++ client/src/components/Product.jsx
@@ -0,0 +1,120 @@
+import React from "react";
+import styled from "styled-components";
+import tw from "twin.macro";
+import { Add, Search } from "@material-ui/icons";
+import { Link, useNavigate } from "react-router-dom";
+
+const Container = tw.div`
+    -mb-16
+    md:mb-5
+    lg:mb-16
+    flex
+    flex-col
+    h-[400px]
+    md:h-[300px]
+    max-w-[100%]
+    w-[40%]
+    md:w-auto
+    relative
+    md:mx-0
+    mx-2
+    items-center
+`;
+
+const Image = tw.img`
+    // h-[250px]
+    md:w-[20vw]
+    
+    // w-full
+    rounded
+    mx-auto
+    
+`;
+const Info = tw.div`
+    flex
+    flex-col
+    justify-center
+    items-center
+    text-center
+    mt-3
+`;
+const Category = tw.div`
+    text-sm
+    font-normal
+    capitalize
+    mt-1
+    text-gray-500
+`;
+
+const Title = tw.div`
+    text-base
+    font-extrabold
+    mt-1
+    capitalize
+    max-w-[80%]
+    text-center
+    flex
+    justify-center
+`;
+
+const Price = tw.div`
+    text-sm
+    font-bold
+    mt-2
+    text-gray-800
+`;
+
+const Colors = tw.div`
+    mt-3
+    flex
+    justify-start
+    items-start
+    justify-items-start
+`;
+
+const Color = styled.div`
+  background-color: ${(props) => props.bg};
+  ${tw`
+        flex
+        justify-center
+        items-center
+        mr-1
+        h-5
+        w-5
+        rounded
+        border-2
+        shadow-inner
+    `}
+`;
+
+export const Product = ({ item }) => {
+  const navigate = useNavigate();
+  return (
+    <Container className="">
+      <Link to={`/products/detail/${item.slug}`}>
+        <Image src={item.data[0].img} alt="" />
+      </Link>
+
+      <Colors>
+        {item.data.slice(0, 4).map((data, index) => (
+          <Color key={index} bg={data.hex} />
+        ))}
+        {item.data.length > 4 && (
+          <Color bg={item.data[4].hex}>
+            <Add className="text-gray-100" style={{ fontSize: 15 }} />
+          </Color>
+        )}
+      </Colors>
+      <Info>
+        <Category>{item.category.name}</Category>
+        <Link
+          className="flex justify-center "
+          to={`/products/detail/${item.slug}`}
+        >
+          <Title>{item.name}</Title>
+        </Link>
+        <Price>{item.price}</Price>
+      </Info>
+    </Container>
+  );
+};
