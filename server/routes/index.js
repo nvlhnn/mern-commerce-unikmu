c87c3bb216b260584247e23154a4d5f1aa1d1342
nvlhnn <hanannaufal17@gmail.com>
2022-04-15 18:18:51 +0700

    3

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,27 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const authRoutes = require('./api/auth');
-const userRoutes = require('./api/user');
-const productRoutes = require('./api/product');
-const categoryRoutes = require('./api/category');
-const cartRoutes = require('./api/Cart');
-const orderRoutes = require('./api/Order');
-const colorRoutes = require('./api/Color');
-
+const authRoutes = require("./api/auth");
+const userRoutes = require("./api/user");
+const productRoutes = require("./api/product");
+const categoryRoutes = require("./api/category");
+const cartRoutes = require("./api/Cart");
+const orderRoutes = require("./api/Order");
+const colorRoutes = require("./api/Color");
 
 // auth routes
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
-router.use('/products', productRoutes);
-router.use('/categories', categoryRoutes);
-router.use('/carts', cartRoutes);
-router.use('/orders', orderRoutes);
-router.use('/colors', colorRoutes);
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/products", productRoutes);
+router.use("/categories", categoryRoutes);
+router.use("/carts", cartRoutes);
+router.use("/orders", orderRoutes);
+router.use("/colors", colorRoutes);
 
+router.use("/api", router);
 
-
-router.use('/api', router)
 // router.get('api/user/stat', (req, res) => res.status(200).json('ok'))
-router.get('/api', (req, res) => res.status(404).json('No API route found'));
+router.get("/api", (req, res) => res.status(404).json("No API route found"));
 
 module.exports = router;
