7f01d3c3bc890f6617a2ff356662f63437fb9b2e
nvlhnn <hanannaufal17@gmail.com>
2022-04-17 23:28:42 +0700

    make it better

--- index.js
+++ index.js
@@ -21,14 +21,14 @@ require("./config/passport")(app);
 app.use(routes);
 
 // ______________deployment_____________
-__dirname = path.resolve();
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+// __dirname = path.resolve();
+// if (process.env.NODE_ENV == "production") {
+app.use(express.static(path.join(__dirname, "/client/build")));
 
-  app.use("/*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
-  });
-}
+app.use("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
+});
+// }
 
 // app.get("/*", (req, res) => res.send("Index Page"));
 
