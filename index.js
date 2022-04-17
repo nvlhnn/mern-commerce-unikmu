b836a2f974659cc5b516a551e022ae2d9d67cb6f
nvlhnn <hanannaufal17@gmail.com>
2022-04-17 23:42:06 +0700

    make it better

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(routes);
 // if (process.env.NODE_ENV == "production") {
 app.use(express.static(path.join(__dirname, "/client/build")));
 
-app.use("*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "/client/build", "index.html"));
 });
 // }
