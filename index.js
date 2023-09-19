3889dc1f69edff74d382cdc0b6a8160a1d6090a3
nvlhnn <hanannaufal17@gmail.com>
2023-09-19 17:40:57 +0700

    f

--- index.js
+++ index.js
@@ -20,13 +20,27 @@ require("./config/passport")(app);
 
 app.use(routes);
 
-// ______________deployment_____________
-// __dirname = path.resolve();
-// if (process.env.NODE_ENV == "production") {
-app.use(express.static("client/build"));
+// // ______________deployment_____________
+// // __dirname = path.resolve();
+// // if (process.env.NODE_ENV == "production") {
+// app.use(express.static("client/build"));
 
+// app.get("*", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+// });
+// // }
+
+// if (process.env.NODE_ENV === "production") {
+app.use(express.static(path.resolve(__dirname, "client", "build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  res.sendFile(
+    path.resolve(__dirname, "client", "build", "index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
 });
 // }
 
